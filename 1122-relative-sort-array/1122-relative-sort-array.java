d21f09adbfcaae06feda1f5a0ff569eb034ae28a
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-04-20 22:41:38 +0530

    Time: 0 ms (100%), Space: 43.7 MB (71.32%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // 1. Max element dhundo taaki count array ki size fix ho sake (max 1000 diya hai constraints mein)
+        int[] count = new int[1001];
+        
+        // 2. arr1 ke elements ki frequency count karo
+        for (int num : arr1) {
+            count[num]++;
+        }
+        
+        int[] result = new int[arr1.length];
+        int idx = 0;
+        
+        // 3. Pehle arr2 ke hisaab se elements ko result mein daalo
+        for (int num : arr2) {
+            while (count[num] > 0) {
+                result[idx++] = num;
+                count[num]--;
+            }
+        }
+        
+        // 4. Ab bache hue elements ko ascending order mein daalo
+        // (count array 0 se 1000 tak iterate karne par apne aap sorted order milega)
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                result[idx++] = i;
+                count[i]--;
+            }
+        }
+        
+        return result;
+    }
+}
