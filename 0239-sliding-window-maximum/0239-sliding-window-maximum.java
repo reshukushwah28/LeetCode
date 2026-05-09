a7f118bdc857bdfb73ed8427419b0d8ef36fa801
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-05-09 23:17:51 +0530

    Time: 32 ms (40.76%), Space: 146.7 MB (63.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,93 +1,28 @@
+import java.util.*;
+
 class Solution {
-
-    class Node {
-        int data;
-        Node prev, next;
-
-        Node(int data) {
-            this.data = data;
-            this.prev = null;
-            this.next = null;
-        }
-    }
-
-    class Deque {
-        Node front, rear;
-
-        boolean isEmpty() {
-            return front == null;
-        }
-
-        void insertrear(int x) {
-            Node n = new Node(x);
-            if (rear == null) {
-                front = rear = n;
-            } else {
-                rear.next = n;
-                n.prev = rear;
-                rear = n;
-            }
-        }
-
-        void deletefront() {
-            if (front == null) return;
-
-            if (front == rear) {
-                front = rear = null;
-            } else {
-                front = front.next;
-                front.prev = null;
-            }
-        }
-
-        void deleterear() {
-            if (rear == null) return;
-
-            if (front == rear) {
-                front = rear = null;
-            } else {
-                rear = rear.prev;
-                rear.next = null;
-            }
-        }
-
-        int getfront() {
-            return front.data;
-        }
-
-        int getrear() {
-            return rear.data;
-        }
-    }
-
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int idx = 0;
-
-        Deque dq = new Deque();
-
+        int[] res = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        
         for (int i = 0; i < n; i++) {
-
-            // remove smaller elements from rear
-            while (!dq.isEmpty() && nums[dq.getrear()] <= nums[i]) {
-                dq.deleterear();
+            
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
             }
-
-            // insert current index
-            dq.insertrear(i);
-
-            // remove out-of-window index
-            if (!dq.isEmpty() && dq.getfront() <= i - k) {
-                dq.deletefront();
+            
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
             }
-
-            // store answer
+            
+            dq.offerLast(i);
+            
             if (i >= k - 1) {
-                ans[idx++] = nums[dq.getfront()];
+                res[i - k + 1] = nums[dq.peekFirst()];
             }
         }
-        return ans;
+        
+        return res;
     }
-    }
-
+}
