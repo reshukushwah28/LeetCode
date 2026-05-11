55628467619ef0cd58178603c3ee09919159c705
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-05-11 21:20:35 +0530

    Time: 4 ms (100%), Space: 45.9 MB (72.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        if(x<0)return false;
+        while(x!=0 ){
+            rev = rev*10 + x%10;
+            x/=10;
+            
+        }
+        return temp == rev;
+    }
+}
