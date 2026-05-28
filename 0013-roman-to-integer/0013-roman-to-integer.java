5ad187756c621921e2f058311920b705087bc321
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-05-29 01:46:46 +0530

    Time: 4 ms (58.26%), Space: 46.3 MB (75.56%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,44 +1,27 @@
 class Solution {
-
     public int romanToInt(String s) {
+        Map<Character, Integer> map = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+        );
 
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        int ans = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-
+        int res = 0;
+        int length = s.length();
+        int prev = 0;
+        for (int i = length - 1; i >= 0; i--) {
             int curr = map.get(s.charAt(i));
-
-            // next character exists
-            if(i < s.length() - 1) {
-
-                int next = map.get(s.charAt(i + 1));
-
-                // subtraction case
-                if(curr < next) {
-
-                    ans -= curr;
-                }
-                else {
-
-                    ans += curr;
-                }
+            if (curr < prev) {
+                res -= curr;
+            } else {
+                res += curr;
             }
-            else {
-
-                ans += curr;
-            }
+            prev = curr;
         }
-
-        return ans;
+        return res;
     }
 }
