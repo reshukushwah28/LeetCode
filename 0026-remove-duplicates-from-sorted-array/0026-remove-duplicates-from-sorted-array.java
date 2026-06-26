a56877fd0f8dd77ae139af202d69fce7d3376130
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-06-26 12:49:00 +0530

    Time: 3 ms (6.37%), Space: 47 MB (8.74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k =1;
-        //kuki peheli hi hme ek unique element mil gya h kuki array soted h to first element is always unique 
-        for(int  i=1;i<nums.length;i++){
-            if(nums[i] != nums[i-1]){
-                nums[k] = nums[i];  
-                k++;         }
-        }
-        return k;
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int num : nums){
+            if(!set.contains(num)){
+                set.add(num);
+                ans.add(num);
 
+            }
+
+        }
+        for(int i =0;i<ans.size();i++){
+            nums[i] = ans.get(i);
+        }
+        return ans.size();
         
     }
 }
